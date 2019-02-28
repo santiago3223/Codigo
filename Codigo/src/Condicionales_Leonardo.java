f9ce6ee8641c2c630ad4e59acb3fa4a35bf487aa
Leonardo <leodo18@gmail.com>
2019-02-27 21:31:51 -0500

    par e impar

---
+++ Codigo/src/Condicionales_Leonardo.java
@@ -0,0 +1,25 @@
+
+public class Condicionales_Leonardo {
+
+	public static void main(String[] args) {
+		int ingreso = 1001;
+		float monto_minimo_para_renta = 1000;
+		// if (1>2)
+		// if ("hola" == "adios")
+		// if (variable1 > variable2)
+		if (ingreso >= monto_minimo_para_renta) {
+			System.out.println("Pagar renta");
+		}else {
+			System.out.println("Recibe bono");
+		}
+		
+		String nombre = "santiago";
+		if (nombre ==  "santiago") {
+			System.out.println("Ingresa");
+		}else {
+			System.out.println("Se retira");
+		}
+		
+	}
+
+}
