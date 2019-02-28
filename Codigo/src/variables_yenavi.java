8279b2f6f9d336368cb6ac237bd3d491a15256f5
Tecsup <Tecsup@LABA311LP14>
2019-02-27 20:49:11 -0500

    Terminado

---
+++ Codigo/src/variables_yenavi.java
@@ -0,0 +1,27 @@
+
+public class variables_yenavi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Escribe un programa Java que realice lo siguiente: 
+		 * declarar una variable N de tipo int, una variable A de tipo double y una variable C de tipo 
+		 * char y asigna a cada una un valor. A continuación muestra por pantalla:
+		El valor de cada variable.*/
+		int N=5;
+		double A=4.5;
+		char C='a';
+		System.out.println("Entero:"+N);
+		System.out.println("Double:"+A);
+		System.out.println("Char:"+C);
+		
+		/*La suma de N + A
+		La diferencia de A - N
+		El valor numérico correspondiente al carácter que contiene la variable C.
+		Si por ejemplo le hemos dado a N el valor 5, a A el valor 4.56 y a C el valor ‘a’, 
+		se debe mostrar por pantalla:*/
+		
+		System.out.println("La suma de N + A= "+ (N+A));
+		System.out.println("La diferencia de A - N= "+ (A-N));
+	}
+
+}
