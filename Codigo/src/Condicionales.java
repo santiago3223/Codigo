9ccbe0e8fa7abc4447a736a92a7d55f178b72b17
Raul <reglatin@gmail.com>
2019-02-27 21:59:46 -0500

    condicionales

--- Codigo/src/Condicionales.java
+++ Codigo/src/Condicionales.java
@@ -12,12 +12,47 @@ public class Condicionales {
 		}else {
 			System.out.println("Recibe bono");
 		}
-		
-		String nombre = "santiago";
-		if (nombre ==  "santiago") {
-			System.out.println("Ingresa");
+
+		int numero = 12354851;
+		if (numero%2==0) {
+			System.out.println("Par");
 		}else {
-			System.out.println("Se retira");
+			System.out.println("Impar");
+		}
+
+		System.out.println(21%5);
+		System.out.println(17%10);
+
+		int edad = 16;
+		String nacionalidad = "italiana";
+
+		
+		
+		if (edad >= 18 && nacionalidad == "peruana") {
+			System.out.println("puede visitar");
+		}else {
+			if (edad < 18) {
+				if (nacionalidad != "peruana") {
+					System.out.println("no puede visitar, nacionalidad");
+				}
+				System.out.println("no puede visitar, menor de edad");
+			}else {
+				System.out.println("no puede visitar, nacionalidad");
+			}	
+		}
+		
+		
+		if (edad >= 18) {
+			if (nacionalidad == "peruana") {
+				System.out.println("puede visitar");
+			}else {
+				System.out.println("no puede visitar, nacionalidad");
+			}
+		}else {
+			System.out.println("no puede visitar, menor de edad");
+			if (nacionalidad != "peruana") {
+				System.out.println("no puede visitar, nacionalidad");
+			}
 		}
 		
 	}
