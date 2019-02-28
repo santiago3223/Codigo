462962734435771202e0b007988ffe94b52c456f
gohan <nahoj1992@gmail.com>
2019-02-27 20:21:39 -0500

    gohan

---
+++ Codigo/src/Variables_Gohan.java
@@ -0,0 +1,61 @@
+public class Variables_Gohan {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+/*Escribe un programa Java que realice lo siguiente: declarar una variable N de tipo int, 
+ * una variable A de tipo double y una variable C de tipo char y 
+ * asigna a cada una un valor. 
+ * A continuación muestra por pantalla:
+El valor de cada variable.
+La suma de N + A
+La diferencia de A - N
+El valor numérico correspondiente al carácter que contiene la variable C.
+Si por ejemplo le hemos dado a N el valor 5, a A el valor 4.56 y a C el valor ‘a’, se debe mostrar por pantalla:
+Variable N = 5
+Variable A = 4.56
+Variable C = a
+5 + 4.56 = 9.559999999999999
+4.56 - 5 = -0.4400000000000004
+Valor numérico del carácter a = 97
+ */
+		int N;
+		double A;
+		char C;
+		N = 1;
+		A = 2.48;
+		C = 'R';
+		double AN = A+N;
+		double NA = N-A;
+		int IC = C;
+		System.out.println("Variable N = "+N);
+		System.out.println("Variable A = "+A);
+		System.out.println("Variable C = "+C);
+		System.out.println(N+" + "+A+" = "+AN);
+		System.out.println(A+" - "+N+" = "+NA);
+		System.out.println( IC );
+		
+		
+		/*Escribe un programa Java que realice lo siguiente: declarar dos variables X e Y de tipo int, dos variables N y M de tipo double y asigna a cada una un valor. A continuación muestra por pantalla:
+El valor de cada variable.
+La suma  X + Y
+La diferencia  X – Y
+El producto  X * Y
+El cociente  X / Y
+El resto  X % Y
+La suma  N + M
+La diferencia  N – M
+El producto  N * M
+El cociente  N / M
+El resto  N % M
+La suma X + N
+El cociente Y / M
+El resto Y % M
+El doble de cada variable
+La suma de todas las variables
+El producto de todas las variables
+*/
+		System.out.println(":)::");
+	}
+
+}
