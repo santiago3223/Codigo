7f8266fca3f16360d1d39e6a04c3a41918dfe821
CarlosMestas <cmestas@unsa.edu.pe>
2019-02-27 20:48:26 -0500

    :v

---
+++ Codigo/src/Variables_CarlosM.java
@@ -0,0 +1,15 @@
+
+public class Variables_CarlosM {
+
+	public static void main(String[] args) {
+		int N = 5;
+		double A = 4.56;
+		char C = 'a';
+		System.out.println("Suma de N + A: "+ (N + A));
+		System.out.println("La diferencia de A - N: "+ (A - N));
+		System.out.println("Valor numérico del caracter a = "+ C);
+	
+		System.out.println(":)");
+	}
+
+}
