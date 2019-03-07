07862ee895260dc1ba877f779edd33212f0a02b0
Tecsup <Tecsup@LABA311LP05>
2019-03-06 21:06:08 -0500

    ejercicios

---
+++ Codigo/src/Ejercicios1_Karem.java
@@ -0,0 +1,125 @@
+
+public class Ejercicios1_Karem {
+
+	public static void main(String[] args) {
+	
+//		int numero = Entrada.entero();
+//		if(numero >=0 && numero<=9)
+//		{
+//		   System.out.println("Es de una cifra"); 
+//		}
+//		else if(numero >=10 && numero<=99)
+//		{
+//		 System.out.println("Es de dos cifra");
+//
+//		}else if(numero >= 100 && numero<=999)
+//		{
+//			System.out.println("Es de tres cifras");
+//		}
+//		else if(numero>=1000 && numero <=9999)
+//		{
+//			System.out.println("Es de cuatro cifras");
+//		}
+//		else
+//		{
+//			System.out.println("Es de más de cuatro cifras");
+//		}
+		
+	//	int dado1=Entrada.entero();
+//		int dado2=Entrada.entero();
+//		int dado3=Entrada.entero();
+//		
+//		if(dado1==6 && dado2==6 && dado3==6)
+//		{
+//			System.out.println("Excelente");
+//		}
+//		else if(dado1==6 && dado2==6 || dado2==6 && dado3==6 || dado1==6 && dado3==6)
+//		{
+//				System.out.println("Muy Bueno");
+//		}else if (dado1==6 || dado2==6 || dado3==6)
+//		{
+//			System.out.println("Regular");
+//		}
+//		else
+//		{
+//			System.out.println("PESIMO");
+//		}
+		
+		
+//		int dia=Entrada.entero();
+//		int mes=Entrada.entero();
+//		int ano=Entrada.entero();
+//		int resultado=dia;
+//		
+//		switch(mes)
+//		{
+//		    case 12:
+//		    	resultado+=30;
+//		    case 11:
+//		    	resultado+=31;
+//		    case 10:
+//		    	resultado+=30;
+//		    case 9:
+//		    	resultado+=31;
+//		    case 8:
+//		    	resultado+=31;
+//		    case 7:
+//		    	resultado+=30;
+//		    case 6:
+//		    	resultado+=31;
+//		    case 5:
+//		    	resultado+=30;
+//		    case 4:
+//		    	resultado+=31;	
+//			case 3:
+//				if(ano%4== 0 && ano%100!=0)
+//				{
+//					resultado+=29;
+//				}
+//				else
+//				{
+//					resultado+=28;
+//				}
+//			case 2:
+//				resultado+=31;
+//			default:
+//				break;
+//		}
+//		
+//		System.out.println("El numero de dias es: "+resultado );
+		
+//		String caracteres="";
+//		for (char c='A'; c<='Z'; c++)
+//		{
+//			caracteres = caracteres + c +"="+(int)c +"\t";
+//		}
+//		System.out.println(caracteres);
+		
+		
+//		int num=Entrada.entero();
+//		int resultado=1;
+//		for(int i=1; i<=num; i++) //	for(int i=1; i<=num; i=i+1)
+//		{
+//			resultado=resultado*i;
+//		}
+//		System.out.println("El factorial de "+num+" es: "+resultado);
+		
+		
+//		int total=1;
+//		for (int pos=1; pos<=10; pos++)
+//		{
+//			int resul=1;
+//			for(int i=1; i<=pos; i++)
+//			{
+//				resul = resul*i;
+//			}
+//			System.out.println("El factorial "+pos+" es "+resul);
+//			total =total+resul;
+//		}
+//		
+//		System.out.println("El resultado total es: "+ total);
+		
+		
+		
+  }
+}
