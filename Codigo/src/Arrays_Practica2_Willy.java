f10f4ca5e38c3b78468fbbb2434147ce3efb0480
Hackalmighty <30963719+Hackalmighty@users.noreply.github.com>
2019-03-12 19:50:37 -0500

    willys

---
+++ Codigo/src/Arrays_Practica2_Willy.java
@@ -0,0 +1,32 @@
+
+public class Arrays_Practica2_Willy {
+
+	public static void main(String[] args) {
+//		Ejercicio 1.- Escriba un programa que permita crear una arreglo de palabras y 
+//		que, a continuación, pida dos palabras y sustituya la primera por 
+//		la segunda en la lista.
+		
+		System.out.print("Cuantas palabras ingresará: ");
+		int tamano = Entrada.entero();
+		String [] Nombres = new String [tamano];
+		for (int i = 0; i < tamano; i++)
+		{
+			System.out.print("Dígame la palabra " + (i + 1) + ": ");
+			Nombres [i] = Entrada.cadena();
+		}
+		String mascota = "";
+		for (int i = 0; i < tamano; i++)
+		{
+			if (i == tamano -1)
+			{
+				mascota = mascota +"'" + Nombres [i] +"'";
+			} else 
+			{
+				mascota = mascota +"'" + Nombres [i] +"', ";	
+			}
+		}
+		System.out.print(temp);
+
+	}
+
+}
